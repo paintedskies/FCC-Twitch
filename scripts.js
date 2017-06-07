9c33d251b93d908eed31b67911264c08f0edbf25
paintedskies <shrutiavinodh@gmail.com>
2017-06-07 22:36:06 +0530

    basic background animations added

--- scripts.js
+++ scripts.js
@@ -4,17 +4,27 @@ $(document).ready(function(){
 	$.each(usernames, function(){getdata(this,"all");});
 	$("button").click(function()
 			{
-				console.log(this);
 				$("button").removeClass("active");
 				$(this).addClass("active");	
 				var mode = this.id;
-		
+
 				$("#channel-list").html("");
 				$.each(usernames, function(index, item){getdata(item,mode);});
+				if(this.id=="offline"){
+					$("body").css({"background":"#20a4f3"});
+				}
+				else if(this.id=="online"){
+					$("body").css({"background":"#ff3366"});
+				}
+				else if(this.id=="all"){
+					$("body").css({"background":"#e7e247"});
+				}
 			});
 
 
+
 });
+
 
 function getdata(username, x)
 {
@@ -30,19 +40,21 @@ function getdata(username, x)
 			var img;
 			if(res.stream.channel.logo == null)
 			{
-				img = "<img src = \"images/unknown.png\">";
+				img = "<img src = \"images/unknown.png\"> ";
 			}
 			else
 			{
-				img = "<img src = \"" + res.stream.channel.logo + "\">";
+				img = "<img src = \"" + res.stream.channel.logo + "\"> ";
+
 			}
-			var details = "<h2>" + res.stream.channel.display_name + "</h2>" + "<p>" + res.stream.game + ": " + res.stream.channel.status + "</p>";
-			$("#channel-list").append("<div class = \"list-item\">" + img + details+"</div>");
+			//console.log(res);
+			var details = "<a href = \"" + res.stream.channel.url + "\" target = \"_blank\"> <h2>" + res.stream.channel.display_name + "</h2> </a>" + "<p>Online</p><hr>"+ "<em><p>" + res.stream.game + ": " + res.stream.channel.status + "</p></em>";
+			$("#channel-list").append("<div class = \"list-item online\">" + img + details+"</div>");
 		}
 		if( (res.stream == null) && (x == "all" || x == "offline") )
 		{
 			$.ajax({
-				url: "https:/wind-bow.glitch.me/twitch-api/users/"+username,
+				url: "https:/wind-bow.glitch.me/twitch-api/channels/"+username,
 				method: "GET",
 				dataType: "jsonp"
 			})
@@ -51,21 +63,21 @@ function getdata(username, x)
 				{
 					var img;
 
-				if(userres.logo == null)
+					if(userres.logo == null)
 					{
-						img = "<img src = \"images/unknown.png\">";
+						img = "<img src = \"images/unknown.png\"> ";
 					}
 					else
 					{
-						img = "<img src = \"" + userres.logo + "\">";
+						img = "<img src = \"" + userres.logo + "\"> ";
 					}
-					
-					var details = "<h2>" + userres.display_name + "</h2>" + "<p>Offline</p>";
-					$("#channel-list").append("<div class = \"list-item\">" + img + details +"</div>");
+					//		console.log(userres);
+					var details = "<a href = \""+userres.url+"\" target = \"_blank\"> <h2>" + userres.display_name + "</h2></a>" + "<p>Offline</p>" + "<hr>";
+					$("#channel-list").append("<div class = \"list-item offline\">" + img + details +"</div>");
 				}
 				else if(x == "all")
 				{		
-					$("#channel-list").append("<div class = \"list-item\">"+"<img src = \"images/unknown.png\">" + "<h2>" + username + "</h2>" + "<p>Account not found.</p>"+"</div>");
+					$("#channel-list").append("<div class = \"list-item not-found\">"+"<img src = \"images/unknown.png\">" + "<h2>" + username + "</h2>"+ "<p>Account not found</p>"+"<hr>"+"</div>");
 				}
 			});
 
